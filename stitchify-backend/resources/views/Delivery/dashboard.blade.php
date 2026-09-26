ba18f41ef7670c8464e8cb6c031b2ddebeae8090
somiasultan-fyp <stitchify2026@gmail.com>
2026-09-26 17:42:13 +0500

    final

--- stitchify-backend/resources/views/Delivery/dashboard.blade.php
+++ stitchify-backend/resources/views/Delivery/dashboard.blade.php
@@ -309,7 +309,7 @@
 </div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script>
-<script src="{{ asset('js/delivery-dashboard.js') }}"></script>
+<script src="{{ asset('js/deliverydashboard.js') }}"></script>
 
 </body>
 </html>
