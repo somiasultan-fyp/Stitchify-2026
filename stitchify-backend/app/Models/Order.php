eddf11c60066c089da468fe13df48591e9cf0380
somiasultan-fyp <stitchify2026@gmail.com>
2026-09-26 18:03:14 +0500

    controller

--- stitchify-backend/app/Models/Order.php
+++ stitchify-backend/app/Models/Order.php
@@ -18,6 +18,7 @@ class Order extends Model
         'recipient_phone',
         'recipient_address',
         'recipient_city',
+        'area',
         'dress_type',
         'special_instructions',
         'fabric_details',
